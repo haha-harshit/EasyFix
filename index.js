0a9853e09562a367b53c1bc7a0ff4bf4d07aa53f
Harshit Kumar <bkharshitbk@gmail.com>
2022-09-17 03:59:32 +0530

    required path

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 8000;
 
 const app = express();
 
+const path = require("path");
+
 app.listen(port, function (err) {
     if (err) {
         console.log(`Error in running the server: ${err}`);
