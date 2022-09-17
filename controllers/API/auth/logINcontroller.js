f228085fb6f54e8f9e25b60c9e65e8819ac9af50
Harshit Kumar <bkharshitbk@gmail.com>
2022-09-18 02:11:42 +0530

    created action for log-in

--- controllers/API/auth/logINcontroller.js
+++ controllers/API/auth/logINcontroller.js
@@ -1,5 +1,14 @@
+const User = require("../../../models/user");
 const {check, validationResult} = require('express-validator')
 
+const env = require('../../../config/environment');
+
+var jwt = require('jsonwebtoken');
+var expressJWT = require('express-jwt');
+
+
+// #1
+// GET log-in page
 module.exports.login = function(req, res){
     console.log('log-in page');
     // return res.send("<h1>Log-In page</h1>")
@@ -8,6 +17,8 @@ module.exports.login = function(req, res){
     })
 }
 
+// #2
+// Action for loggin-in
 module.exports.create_session = (req, res) => {
     const errors = validationResult(req)
     if(!errors.isEmpty()){
@@ -16,7 +27,31 @@ module.exports.create_session = (req, res) => {
         })
     }
     console.log("creating session - login");
-    return res.json({
-        success: "login success"
+    
+    User.findOne({email:req.body.email}, function(err, user){
+        if(err){
+            return res.status(400).json({
+                error: "User E-mail does not exist!"
+            })
+        }
+        if(!user.authenticate(req.body.password)){
+            // console.log(req.email)
+            // console.log(req.body.password)
+            return res.status(401).json({
+                error: "E-mail and password did not match!"
+            })
+        }
+        if(user.authenticate(req.body.password)){
+            // create token
+            const token = jwt.sign({_id: user._id}, env.SECRET)
+
+            // put token in cookie
+            res.cookie("token", token)
+
+            // send response to front end
+            const{ _id, username, email, role} = user;
+            return res.json({token , user: {_id, username, email, role}});
+        }
     })
+    
 }
