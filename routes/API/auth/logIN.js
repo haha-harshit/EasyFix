a881806e4fc5845c3103ca25a37619b62b149ddb
Harshit Kumar <bkharshitbk@gmail.com>
2022-09-17 16:36:48 +0530

    express validation for login-signup post req

--- routes/API/auth/logIN.js
+++ routes/API/auth/logIN.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
+const {check, validationResult} = require('express-validator');
 const passport = require('passport');
 
 
 // log-in controller
 const loginController = require('../../../controllers/API/auth/logINcontroller')
 
+// GET LOGIN page
 router.get('/', loginController.login)
+
+// LOGIN-session
+router.post('/log-in', [
+    check("email", "E-mail is required").isEmail(),
+    check("password", "Password is required").isLength({min: 1})
+]);
 
 module.exports = router;
--- routes/API/auth/signUP.js
+++ routes/API/auth/signUP.js
@@ -1,14 +1,21 @@
 const express = require('express');
-
 const router = express.Router();
-
+const {check, validationResult} = require('express-validator');
 const passport = require('passport');
 
 
 // sign-up controller
 const signupController = require('../../../controllers/API/auth/signUPcontrollers')
 
+// GET SIGN-UP FORM
 router.get('/', signupController.signup)
 
+// POST SIGN-UP request || create user
+router.post('/sign-up', [
+    // express-validation
+    check("username", "Name must be at least 3 chars long").isLength({ min: 3 }),
+    check("email", "E-mail is required").isEmail(),
+    check("password", "Password must be atleast 4 char").isLength({min: 4}) 
+]);
 
 module.exports = router;
