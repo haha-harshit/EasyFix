4ebce59322b2c0b39445fd0917daeb5cd0efd556
Harshit Kumar <bkharshitbk@gmail.com>
2022-09-18 10:30:49 +0530

    setup passport-jwt-strategy

---
+++ config/passport-jwt-strategy.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const extractJWT = require('passport-jwt').ExtractJwt;
+
+const env = require('../config/environment');
+
+const User = require("../models/user");
+
+let opts = {
+    jwtFromRequest: extractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: env.SECRET
+}
+
+passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){
+
+    User.findById(jwtPayLoad._id, function(err, user){
+        if(err){
+            console.log('Error in finding user from JWT');
+            return;
+        }
+        if(user){
+            return done(null,user);
+        }
+        else{
+            return done(null, false);
+        }
+    })
+
+}));
+
+module.exports = passport;
