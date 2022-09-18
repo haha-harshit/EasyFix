8bec9da974861489a4e7d72f23c560dfae225f41
Harshit Kumar <bkharshitbk@gmail.com>
2022-09-18 10:26:54 +0530

    setup passport-local-strategy

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,74 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/user');
+
+// authenticating USER using passport
+passport.use('local' ,new LocalStrategy({
+        usernameField: 'email',
+        passReqToCallback: true
+    }, 
+    function(req, email, password, done){
+        // find a user and establish the identity
+        User.findOne({email: email}, function(err, user){
+            if(err){
+                req.flash('error', err);
+                return done(err);
+            }
+            else if(!user || user.password != password){
+                req.flash('error', 'Invalid Username/Password');
+                // console.log('Not a student id!');
+                return done(null, false);
+            }
+            else{
+                return done(null, user);
+            }
+        }) 
+    }
+));
+
+
+//serializing the user to decide which key is to be kept in the cookies
+passport.serializeUser(function(user, done){
+    console.log('serialize done');
+    done(null, user.id);
+});
+
+
+//De-serializing the user from the key in the cookies
+passport.deserializeUser(function(id, done){
+    User.findById(id, function(err, user){
+        if(err){
+            console.log('Error in finding User! ---> passport')
+            return done(err);
+        }
+        console.log('de-serialize done');
+        return done(null, user);
+    })
+});
+
+
+// check if user is authenticated
+passport.checkAuthentication = function(req, res, next){
+    // if user is signed in then pass on the next action
+    if(req.isAuthenticated()){
+        return next();
+    }
+
+    // if user is not signed in
+    req.flash('information', 'You are not Logged In!');
+    return res.redirect('back');
+}
+
+passport.setAuthenticatedUser = function(req, res, next){
+    if(req.isAuthenticated){
+        // req.user contains the current signed in user from the session cookie and sending this to the locals for views
+        res.locals.user = req.user;
+    }
+    next();
+}
+
+
+
+module.exports = passport;
