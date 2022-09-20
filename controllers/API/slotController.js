0df3387d5d538c131acee97b1d42c842e66fd07a
Harshit Kumar <bkharshitbk@gmail.com>
2022-09-20 11:46:23 +0530

    action for book-appointment setup

--- controllers/API/slotController.js
+++ controllers/API/slotController.js
@@ -31,3 +31,25 @@ module.exports.appointment_plannar = async function(req, res){
         return res.redirect('back')
     }
 }
+
+module.exports.book_appointment = async function(req, res){
+    try {
+        if (req.isAuthenticated()) {
+            let guest = await User.findById(req.params.id);
+            let client = await User.findById(req.user._id);
+
+            if(guest && client){
+                // guest.upcoming_appointments.push(req.body.datetimes)
+            }
+
+
+            return res.render('_appointment_plannar', {
+                title: 'EasyFix | Appointment Plannar',
+                guest: guest
+            })
+        }    
+    } catch (error) {
+        console.log('error in appoint-plannar', error);
+        return res.redirect('back')
+    }
+}
--- routes/API/slot.js
+++ routes/API/slot.js
@@ -6,11 +6,16 @@ const passport = require('passport');
 // slot controller
 const slotController = require('../../controllers/API/slotController')
 
+// #1
 // mark off-hours
 router.post('/mark-as-off', passport.checkAuthentication, slotController.markAsOffHours)
 
-
+// #2
 // GET appointment page
 router.get('/appointment-plannar/:id', passport.checkAuthentication, slotController.appointment_plannar)
+
+// #3
+// POST book appointment
+router.post('/book-appointment/:id', passport.checkAuthentication, slotController.book_appointment)
 
 module.exports = router;
